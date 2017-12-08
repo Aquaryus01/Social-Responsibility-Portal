c08f054d206fe4444ea627ba43b0c85c1df909cd
SoulTaku <zombie2912@yahoo.com>
2017-12-08 20:49:03 +0200

    Backend: Added get_location and get_issues

--- Backend/server.py
+++ Backend/server.py
@@ -2,12 +2,15 @@
 import sqlite3
 import jwt
 import json
+import os
 from flask import Flask, request
 from flask_cors import CORS
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 CORS(app)
+UPLOAD_FOLDER = '/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 jwt_key = 'OQU6kcW1J0Y0jG9uVnU5hznryLm1df7bMvuM30GY8Im6_RsmMv5fvW5pcft1QFYk'
 
@@ -28,8 +31,6 @@ def login():
     except Exception as e:
         print(e)
         return ''
-        
-
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -44,11 +45,54 @@ def register():
               data['age'])
     print(data, '\n', to_add)
     try:
-        c.execute('INSERT INTO Users (firstName, lastName, email, password, gender, location, age) VALUES (?, ?, ?, ?, ?, ?, ?)', to_add)
+        c.execute('INSERT INTO Users \
+            (firstName, lastName, email, password, gender, location, age) \
+            VALUES (?, ?, ?, ?, ?, ?, ?)', to_add)
         con.commit()
         return '1'
     except Exception as e:
         print(e)
         return '0'
 
-##@app.route('/get', methods=[''])
+@app.route('/post_issue', methods=['POST'])
+def post_issue():
+    data = request.get_json(force=True)
+    if 'file' not in request.files:
+        return 'No file!'
+    file = request.files['file']
+    decoded_jwt = jwt.decode(data['jwt'], jwt_key)
+    print(decoded_jwt)
+    saved_img = os.path.join(app.config['UPLOAD_FOLDER'],
+                           decoded_jwt['userId'] + '-' + file.filename)
+    file.save(os.path.join(saved_img))
+    to_add = (decoded_jwt['userId'],
+              data['title'],
+              data['description'],
+              saved_img)
+    try:
+        c.execute('INSERT INTO Issues (userId, title, description, image) \
+            VALUES (?, ?, ?, ?)', to_add)
+        con.commit()
+    except Exception as e:
+        print(e)
+        return ''
+
+    return '1'
+
+@app.route('/get_location', methods=['GET'])
+def get_location():
+    data = request.get_json(force=True)
+    decoded_jwt = jwt.decode(data['jwt'], jwt_key)
+    try:
+        c.execute('SELECT location FROM Users WHERE userId = ?',
+                  (decoded_jwt['userId'],))
+    except Exception as e:
+        print(e)
+        return ''
+
+    return c.fetchone()[0]
+        
+    
+
+
+    
